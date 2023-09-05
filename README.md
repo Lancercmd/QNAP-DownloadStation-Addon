e49cf5a580b0206c3b73c95c77ba6646a665cf2d
Lancercmd <lancercmd@gmail.com>
2023-09-05 14:19:48 +0800

    🔰 Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# QNAP Download Station Addon
+Unofficial QNAP Download Station Addon repository for additional torrent providers.
+## 蜜柑计划 - Mikan Project
+Download Station ≥ 5.0.0
+
+Download Station 5 的 WebUI 侧，其 BT 搜索看起来是不应用代理设置的，所以如果没有路由器科学上网应该是刷不出东西的。
+
+所以我自己在 WebUI 上也用不了，哎哎哎。
+| 功能 | 实现 |
+| - | :-: |
+| RSS 摘要解析 | - |
+| 搜索 | ✅ |
+| 网址解析| - |
+## 直接安装
+因为没法正确地生成密钥对，所以打包不了，幸好手动安装也不麻烦。
+## 手动安装
+1. 你有一个已安装了 Download Station 5 的 QNAP NAS。
+1. 将 mikanani.me 文件夹拷贝到 NAS 上的位置比如 Public 文件夹。
+1. 通过 SSH 连接到 QNAP NAS。
+1. 将 mikanani.me 文件夹拷贝到插件目录。
+```bash
+# 示例为 QTS 5.1.1.2491
+cp -r /share/Public/mikanani.me /share/CACHEDEV1_DATA/.qpkg/DownloadStation/usr/sbin/addons/mikanani.me
+```
+最后在 Download Station 5 - 设置 - 附加组件 点击更新，新添加的组件就会出现，可以通过编辑对应项来启用或停用组件。
---
+++ mikanani.me/mikanani.me.php
@@ -0,0 +1,137 @@
+<?php
+class mikananime implements ISite, IRss, ISearch, IVerify, IDownload, IPostProcess {
+    const SITE = "https://mikanani.me";
+
+    /*
+     * mikananime()
+     * @param {string} $url
+     * @param {string} $username
+     * @param {string} $password
+     * @param {string} $meta
+     */
+    public function __construct($url = null, $username = null, $password = null, $meta = NULL) {
+    }
+
+    /*
+     * ReadRss()
+     * @return {array} RssFeed array
+     */
+    public function ReadRss() {
+    }
+
+    static function CurlGet($url) {
+
+        // 测试下来只有 curl 可以正常通过代理访问
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        $file_contents = curl_exec($ch);
+        curl_close($ch);
+
+        // 编码转换
+        $file_contents = html_entity_decode($file_contents, ENT_QUOTES, "UTF-8");
+
+        return $file_contents;
+    }
+
+    static function Precision_Size_String_to_Bytes_Integer($size) {
+        $size = trim($size);
+        $unit = strtoupper(substr($size, -2));
+        $size = floatval($size);
+        switch ($unit) {
+            case "KB":
+                return intval($size * 1024);
+            case "MB":
+                return intval($size * 1048576);
+            case "GB":
+                return intval($size * 1073741824);
+            case "TB":
+                return intval($size * 1099511627776);
+            default:
+                return intval($size);
+        }
+    }
+
+    static function Datetime_String_to_PHP_DateTime($datetime) {
+        return new DateTime("$datetime +0800");
+    }
+
+    /*
+     * Search()
+     * @param {string} $keyword
+     * @param {integer} $limit
+     * @param {string} $category
+     * @return {array} SearchLink array
+     */
+    public function Search($keyword, $limit, $category) {
+        $url = mikananime::SITE . "/Home/Search?searchstr=$keyword";
+        $found = array();
+
+        // 加载页面
+        $file_contents = mikananime::CurlGet($url);
+
+        // 使用 XPath 定位
+        $dom = new DOMDocument();
+        @$dom->loadHTML($file_contents, LIBXML_NOERROR | LIBXML_NOWARNING);
+        $xpath = new DOMXPath($dom);
+        $query = $xpath->query('//*[@id="sk-container"]/div[2]/table/tbody/tr');
+
+        // 依次提取搜索结果
+        foreach ($query as $node) {
+            $link = new SearchLink();
+            $link->src = mikananime::SITE;
+            $link->link = $link->src . $node->getElementsByTagName("a")->item(0)->getAttribute("href");
+            $link->name = $node->getElementsByTagName("a")->item(0)->nodeValue;
+
+            $size = $node->getElementsByTagName("td")->item(1)->nodeValue;
+            $link->size = mikananime::Precision_Size_String_to_Bytes_Integer($size);
+
+            $datetime = $node->getElementsByTagName("td")->item(2)->nodeValue;
+            $link->time = mikananime::Datetime_String_to_PHP_Datetime($datetime);
+
+            // $link->seeds = 0;
+            // $link->peers = 0;
+            $link->category = "动画";
+            $link->enclosure_url = $node->getElementsByTagName("a")->item(1)->getAttribute("data-clipboard-text");
+
+            $found[] = $link;
+        }
+
+        return $found;
+    }
+
+    /*
+     * Verify()
+     * @return {boolean}
+     */
+    public function Verify() {
+    }
+
+    /*
+     * GetDownloadLink()
+     * @return {mixed} DownloadLink object or DownloadLink array
+     */
+    public function GetDownloadLink() {
+    }
+
+    /*
+     * RefreshDownloadLink()
+     * @param {DownloadLink} $dlink
+     * @return {DownloadLink} DownloadLink object
+     */
+    public function RefreshDownloadLink($dlink) {
+        return $dlink;
+    }
+
+    /*
+     * PostProcess()
+     * @param {string} $path download temp path
+     * @param {array} $files temp file list
+     * @param {string} $meta caller metadata
+     * @return {array} file list in the $path
+     */
+    static public function PostProcess($path, $files, $meta) {
+        return $files;
+    }
+}
